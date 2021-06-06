135a20de5a9fd33f9408e436f66c3ce05f5ab8af
wanwan <waan@sellsuki.com>
2021-06-06 23:58:11 +0700

    fix: remove import font & add example env

--- app/styles/Global.tsx
+++ app/styles/Global.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;700&display=swap');
-
   * {
     font-family: 'Baloo 2', 'Kanit', sans-serif;
   }
